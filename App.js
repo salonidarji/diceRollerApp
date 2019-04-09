1332238c49f513e8f883a18e44701772ea2a8238
salonidarji <salonidarji@gmail.com>
2019-04-09 13:15:07 +0530

    basic customization done in app.js

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { React } from "react";
+import { View, Text } from "react-native";
+
 
 export default class App extends React.Component {
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app</Text>
-      </View>
+    return(
+      <View>
+      <Text>
+        My Custom App!!!
+      </Text>
+    </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
