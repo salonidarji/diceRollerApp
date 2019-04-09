8481c1c6b1271fe2cc0f160dc82b2606e35b23cd
salonidarji <salonidarji@gmail.com>
2019-04-09 14:11:38 +0530

    completed learning Styles

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import { React } from "react";
-import { View, Text } from "react-native";
-
+/*import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});  */
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export default class App extends React.Component{
   render() {
     return(
-      <View>
-      <Text>
+      <View style={styles.container}>
+      <Text style={styles.textStyle}>
         My Custom App!!!
       </Text>
     </View>
@@ -15,3 +36,16 @@ export default class App extends React.Component {
   }
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#013243",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textStyle: {
+    fontSize: 36,
+    color: "#c5eff7"
+  }
+});
