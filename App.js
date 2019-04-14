0e7e73c9393ffcdb14f3dc7001340649ea9656da
salonidarji <salonidarji@gmail.com>
2019-04-14 15:51:02 +0530

    dice rolling done with one dice

--- App.js
+++ App.js
@@ -3,16 +3,53 @@ import { View, Text, StyleSheet , Image ,TouchableOpacity} from 'react-native';
 
 
 export default class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      uri: require('./assets/DiceAssets/dice1.png')
+    };
+    
+  }
+
+  getRandomNumber = () => {
+    return Math.floor(Math.random() * 6 + 1);
+  }
 
   playButton= () => {
-    alert("hello");
+    var rNumber = this.getRandomNumber();
+
+    switch (rNumber) {
+      case 1:
+        this.setState({ uri: require('./assets/DiceAssets/dice1.png') });
+      break;
+      case 2:
+        this.setState({ uri: require('./assets/DiceAssets/dice2.png') });
+      break;
+      case 3:
+        this.setState({ uri: require('./assets/DiceAssets/dice3.png') });
+      break;
+      case 4:
+        this.setState({ uri: require('./assets/DiceAssets/dice4.png') });
+      break;
+      case 5:
+        this.setState({ uri: require('./assets/DiceAssets/dice5.png') });
+      break;
+      case 6:
+        this.setState({ uri: require('./assets/DiceAssets/dice6.png') });
+      break;
+    
+      default:
+        this.setState({ uri: require('./assets/DiceAssets/dice1.png') });
+        break;
+    }
+
   }
 
   render() {
     return(
       <View style={styles.container}>
       <Image 
-      source={require('./assets/DiceAssets/dice1.png')}
+      source={this.state.uri}
         />
       <TouchableOpacity onPress={this.playButton}>
         <Text style={styles.gameButton}>
